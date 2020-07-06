cf55a137ed9a760ba4f708990bb26fa9c95bc16b
Gatoisi <g_toman.github.com>
2020-07-05 23:49:36 -0400

    first commit

--- src/components/test2.vue
+++ src/components/test2.vue
@@ -1,43 +1,23 @@
 <template>
     <div class="fondo">
         <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div id="mi-slider" class="carousel slide" data-ride="carousel">
-
-                        <!-- indicadores -->
-
-
-                        <!-- wrappers-->
-
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item .active">
-                                <img src="../assets/vestido.jpg" alt="vestido"/>
-                                <div class="carousel-caption">
-                                    <h1>Vestidos</h1>
-                                </div>
-                            </div>
-                            <div class="item ">
-                                <img src="../assets/traje.jpg" alt="vestido"/>
-                                <div class="carousel-caption">
-                                    <h1>Trajes</h1>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- controles -->
-
-                        <a class="left carousel-control" href="#mi-slider" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                            <span class="sr-only"> previous </span>
-                        </a>
-                         <a class="right carousel-control" href="#mi-slider" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                            <span class="sr-only"> previous </span>
-                        </a>
-
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="../assets/vestido.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="../assets/traje.jpg" class="d-block w-100" alt="...">
                     </div>
                 </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
             </div>
         </div>
     </div>
